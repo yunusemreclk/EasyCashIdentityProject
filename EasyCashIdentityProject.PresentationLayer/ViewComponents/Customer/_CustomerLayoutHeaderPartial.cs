f5edffe3f9d9a5029234fd5ad0650e5c095d8f61
YUNUS EMRE ÇELİK <yunusemree60@gmail.com>
2023-05-31 12:29:38 +0300

    ProfilePageAndLoginMVC-UseViewComponent

---
+++ EasyCashIdentityProject.PresentationLayer/ViewComponents/Customer/_CustomerLayoutHeaderPartial.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashIdentityProject.PresentationLayer.ViewComponents.Customer
+{
+    public class _CustomerLayoutHeaderPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ EasyCashIdentityProject.PresentationLayer/ViewComponents/Customer/_CustomerLayoutScriptPartial.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashIdentityProject.PresentationLayer.ViewComponents.Customer
+{
+    public class _CustomerLayoutScriptPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        { return View(); }
+    }
+}
