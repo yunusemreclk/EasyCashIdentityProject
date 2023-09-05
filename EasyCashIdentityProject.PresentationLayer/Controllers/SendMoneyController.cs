88b171e192150c18ec84055c598665b87eed90ba
YUNUS EMRE ÇELİK <yunusemree60@gmail.com>
2023-09-06 00:04:34 +0300

    SendMoney

---
+++ EasyCashIdentityProject.PresentationLayer/Controllers/SendMoneyController.cs
@@ -0,0 +1,54 @@
+﻿using EasyCashIdentityProject.BusinessLayer.Abstract;
+using EasyCashIdentityProject.DataAccessLayer.Concrete;
+using EasyCashIdentityProject.DtoLayer.Dtos.CustomerAccountProcessDtos;
+using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashIdentityProject.PresentationLayer.Controllers
+{
+    public class SendMoneyController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly ICustomerAccountProcessService _customerAccountProcessService; 
+
+        public SendMoneyController(UserManager<AppUser> userManager, ICustomerAccountProcessService customerAccountProcessService)
+        {
+            _userManager = userManager;
+            _customerAccountProcessService = customerAccountProcessService;
+        }
+
+        [HttpGet]
+        public IActionResult Index(string mycurrency)
+        {
+            ViewBag.currency = mycurrency;
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(SendMoneyForCustomerAccountProcessDto sendMoneyForCustomerAccountProcessDto)
+        {
+            var context = new Context();
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var receiverAccountNumberId = context.CustomerAccounts.Where
+              (x => x.CustomerAccountNumber == sendMoneyForCustomerAccountProcessDto.ReciverAccountNumber).Select
+              (y => y.CustomerAccountID).FirstOrDefault();
+
+            //sendMoneyForCustomerAccountProcessDto.SenderID = user.Id;
+            //sendMoneyForCustomerAccountProcessDto.ProcessDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            //sendMoneyForCustomerAccountProcessDto.ProcessType = "Havale";
+            //sendMoneyForCustomerAccountProcessDto.ReceiverID = receiverAccountNumberId;
+            var senderAccountNumberId = context.CustomerAccounts.Where(x => x.AppUserId == user.Id).Where(y => y.CustomerAccountCurrency == "Türk Lirası").Select(z => z.CustomerAccountID).FirstOrDefault();
+            var values = new CustomerAccountProcess();
+            values.ProcessDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            values.SenderID = 1;
+            values.ProcessType = "Havale";
+            values.ReceiverID = receiverAccountNumberId;
+            values.Amount = sendMoneyForCustomerAccountProcessDto.Amount;
+            values.Description = sendMoneyForCustomerAccountProcessDto.Description;
+            _customerAccountProcessService.TInsert(values);
+
+
+            return RedirectToAction("Index","Deneme");
+        }
+    }
+}
